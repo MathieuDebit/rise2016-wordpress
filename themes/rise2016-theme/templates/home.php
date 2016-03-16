41aeb92c76457e7fab6605069c72be3e6c88a57b
Mathieu Débit <mathieu.debit@gmail.com>
2016-03-16 10:25:02 +0100

    Initial commit :cloud: :umbrella:
    * `wp-content`
    * Ignore `uploads` folder

---
+++ themes/rise2016-theme/templates/home.php
@@ -0,0 +1,237 @@
+<?php /* Template Name: Home */ ?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>RISE 2016</title>
+		<meta charset=utf-8 content="IE=edge" http-equiv=X-UA-Compatible />
+		<meta content="width=device-width, initial-scale=1" name=viewport />
+    <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/css/templates/home.css" media="screen" />
+	</head>
+	<body>
+		<div class="modal fade" id=myModal role=dialog>
+			<div class=modal-dialog>
+				<div class=modal-content>
+					<div class=modal-header>
+						<button class=close data-dismiss=modal type=button> &times;</button>
+						<h4 class=modal-title>Restez informé !</h4>
+					</div>
+					<div class=modal-body>
+						<div class=container-fluid>
+							<div class=row id=mc_embed_signup>
+								<form action="//pejfrance.us11.list-manage.com/subscribe/post?u=3e58b0537460580167878546a&amp;id=ad12ab9fce" id=mc-embedded-subscribe-form name=mc-embedded-subscribe-form novalidate=novalidate>
+									<div class=form-group>
+										<div class="col-sm-12 col-xs-12">
+											<div id=mce-responses>
+												<div class=response id=mce-error-response style="display:none"></div>
+												<div class=response id=mce-success-response style="display:none"></div>
+											</div>
+										</div>
+									</div>
+									<div class=form-group>
+										<div class="col-sm-6 col-xs-12"><input class="form-control mobile-margin" id=mce-FNAME name=FNAME placeholder="Prénom"/></div>
+										<div class="col-sm-6 col-xs-12"><input class=form-control id=mce-LNAME name=LNAME placeholder=Nom /></div>
+									</div>
+									<div class=form-group>
+										<div class="col-sm-12 col-xs-12"><input class=form-control id=mce-EMAIL name=EMAIL placeholder=Email type=email /></div>
+									</div>
+									<div class=form-group>
+										<div class="col-sm-12 col-xs-12"><span>Langue de la newsletter :</span><label class="radio-inline mobile-radio-block"><input checked id="mce-group[7597]-7597-0" name="group[7597]" type=radio value=1 />Français</label><label class="radio-inline mobile-radio-block"><input id="mce-group[7597]-7597-1" name="group[7597]" type=radio value=2 />English</label><label class="radio-inline mobile-radio-block"><input id="mce-group[7597]-7597-2" name="group[7597]" type=radio value=4 />Deutsch</label></div>
+									</div>
+									<div class=form-group>
+										<div class="col-sm-12 col-xs-12">
+											<div style="position: absolute; left: -5000px;"><input name=b_3e58b0537460580167878546a_ad12ab9fce tabindex=-1 value=""/></div>
+											<input class="btn btn-primary pull-right" id=mc-embedded-subscribe name=subscribe type=submit value=Valider />
+										</div>
+									</div>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<header id=header>
+			<div class=container>
+				<div class="col-md-12 top_bar">
+					<div class=logo><img src="<?php bloginfo('template_url'); ?>/images/home/header-logo.gif"/></div>
+					<div class=lang><a href="/en">en</a><a href="/de">de</a></div>
+				</div>
+				<div class="col-md-6 col-md-push-2 header-text">
+					<h1 class="wow slideInUp">Rise 2016</h1>
+					<h2 class="wow slideInUp">La 82<span class=exp>e</span> Session Internationale</h2>
+					<p class="wow slideInUp" data-wow-delay="0.2s">Rennes International Session of the EYP ou RISE 2016 est la 82ème Session internationale du Parlement Européen des Jeunes. Elle se déroulera du 14 au 23 juillet 2016 à Rennes, en Bretagne.</p>
+					<button class="btn btn-primary wow slideInUp" data-target="#myModal" data-toggle=modal data-wow-delay="0.3s">Inscrivez-vous à la newsletter</button>
+				</div>
+			</div>
+		</header>
+		<nav class="nav navbar-default" id=nav>
+			<div class=container>
+				<div class=navbar-header><button aria-expanded=false class="navbar-toggle collapsed" data-target="#navbar" data-toggle=collapse><span class=sr-only>Toggle navigation</span><span class=icon-bar></span><span class=icon-bar></span><span class=icon-bar></span></button><a class=navbar-brand href="#header"><img class=logo-menu src="<?php bloginfo('template_url'); ?>/images/home/menu-logo.png"/></a></div>
+				<div class="collapse navbar-collapse" id=navbar>
+					<ul class="nav navbar navbar-right">
+						<li><a href="#session">la session</a></li>
+						<li><a href="#partners">partenaires</a></li>
+						<li><a href="#contact">contact</a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+		<section id=session>
+			<div class=container>
+				<h2>la session</h2>
+				<div class="col-md-8 col-md-push-2">
+					<div class=video><iframe allowfullscreen="" frameborder=0 height=411 mozallowfullscreen="" src="https://player.vimeo.com/video/138024530?color=e7983e&byline=0&portrait=0" webkitallowfullscreen="" width="100%"></iframe></div>
+				</div>
+				<div class="content col-md-6 col-xs-12 col-md-push-3">
+					<p>RISE 2016, la 4ème Session internationale que la France accueille et organise. Un tel événement est un <strong>cadre unique d’échange et de débats</strong> sur les problématiques européennes d’actualité. C’est une véritable simulation parlementaire qui aura lieu pendant ces 10 jours où plus de<strong> 300 jeunes</strong> venus de toute l’Europe pourront s’exprimer et débattre sur des sujets aussi variés que ceux traités au sein du Parlement Européen. <br> <br> Cet événement sera <strong>unique</strong> en son genre, de part son ampleur et son <strong>ouverture sur l’international</strong> mais aussi de par son thème décliné en plusieurs activités. C’est la <strong>diversité</strong> qui sera à l’honneur en juillet 2016. C’est ainsi que la 82ème session internationale ouvrira 25 % de ses places à des jeunes européens qui n’ont jamais participé aux événements du PEJ. Qu’ils soient jeunes professionnels ou étudiants, ces participants seront sélectionnés sur plusieurs critères fixés par l’équipe d’organisation et les comités nationaux du PEJ, dans l’espoir de réunir un public novateur et talentueux. Pour la première fois au PEJ, une session aura <strong>trois langues officielles</strong> : l’anglais, l’allemand et le français. Diversité culturelle, culinaire, politique, artistique, des parcours, etc… <br>Tout sera mis en place pour honorer le devise de l’Union Européenne “Unie dans la diversité”.</p>
+				</div>
+				<div class=clearfix></div>
+				<div class=quote>
+					<div class="col-md-4 col-md-push-1 president wow slideInLeft col-sm-4"><img src="<?php bloginfo('template_url'); ?>/images/home/president.png"/></div>
+					<div class="col-md-6 col-md-push-1 col-sm-8">
+						<blockquote class="blockquote-reverse wow slideInRight">
+							<p>J'ai le grand honneur de présider une Session internationale et je suis comblé d'avoir la chance de présider RISE 2016. <br/> A Rennes, les paroles vides et les effets spéciaux inutiles n'auront pas leur place. Nous essaierons d'utiliser concrètement la diversité de la session en appliquant à de nouveaux contextes les techniques que nous maîtrisons, et en développant de nouvelles pratiques dans les domaines où nous devons nous améliorer. Peut-on rêver d'un meilleur endroit pour "réinventer" notre organisation qu'en France, là où le PEJ est né en 1987 ?</p>
+							<footer>Lorenzo Parrulli, Président de la Session internationale</footer>
+						</blockquote>
+					</div>
+				</div>
+			</div>
+		</section>
+		<div id=counters>
+			<div class=container>
+				<div class="count col-md-4 col-sm-4">
+					<p class=number>10</p>
+					<span>jours de débat</span>
+				</div>
+				<div class="count col-md-4 col-sm-4">
+					<p class=number>39</p>
+					<span>nationalités</span>
+				</div>
+				<div class="count col-md-4 col-sm-4">
+					<p class=number>3</p>
+					<span>langues officielles de travail</span>
+				</div>
+			</div>
+		</div>
+		<section id=partners>
+			<div class=container>
+				<h2>nos partenaires</h2>
+				<div class=haut_patronage>
+					<img src="<?php bloginfo('template_url'); ?>/images/home/partners/logo-elysee.png"/>
+					<p>Sous le Haut Patronage de <br> Monsieur <strong>François HOLLANDE</strong> <br> Président de la République</p>
+				</div>
+				<div id=partnersTab>
+					<ul class="nav nav-tabs col-md-6 col-xs-12">
+						<li class=active><a data-target="#partner-1" data-toggle=tab><img src="<?php bloginfo('template_url'); ?>/images/home/partners/logo-erasmus-plus.png"/></a></li>
+						<li><a data-target="#partner-2" data-toggle=tab><img src="<?php bloginfo('template_url'); ?>/images/home/partners/logo-parlement-europeen.png"/></a></li>
+						<li><a data-target="#partner-3" data-toggle=tab><img src="<?php bloginfo('template_url'); ?>/images/home/partners/logo-commission-europeene.png"/></a></li>
+						<li><a data-target="#partner-4" data-toggle=tab><img src="<?php bloginfo('template_url'); ?>/images/home/partners/logo-ministere-sport.png"/></a></li>
+						<li><a data-target="#partner-5" data-toggle=tab><img src="<?php bloginfo('template_url'); ?>/images/home/partners/logo-ministere-education.png"/></a></li>
+						<li><a data-target="#partner-6" data-toggle=tab><img src="<?php bloginfo('template_url'); ?>/images/home/partners/logo-ministere-affaires-etrangeres.png"/></a></li>
+						<li><a data-target="#partner-7" data-toggle=tab><img src="<?php bloginfo('template_url'); ?>/images/home/partners/logo-fondation-hippocrene.png"/></a></li>
+					</ul>
+					<div class="tab-content col-md-6 col-xs-12">
+						<div class="tab-pane active" id=partner-1>
+							<h3>Erasmus +</h3>
+							<p>Le programme Erasmus + regroupe l’ensemble des actions de l’UE qui investissent dans l'éducation, la formation et la jeunesse pour une croissance durable, intelligente et inclusive. Le programme Erasmus + soutient la 82ème session internationale à travers une subvention allouée dans le cadre des objectifs clés du programme.</p>
+						</div>
+						<div class=tab-pane id=partner-2>
+							<h3>Parlement européen</h3>
+							<p>Le Parlement Européen est l’organe parlementaire de l’Union Européenne. Malgré son nom, le Parlement Européen des Jeunes n’est pas directement mandaté par le Parlement Européen. Toutefois, l’association reçoit une reconnaissance institutionnelle de ses actions par le Parlement Européen.</p>
+						</div>
+						<div class=tab-pane id=partner-3>
+							<h3>Commission européenne</h3>
+							<p>La Commission européenne est la branche exécutive de l’Union Européenne. C’est l’organe qui assure la gestion de l’allocation des financements de l’Union Européenne, notamment les subventions allouées à travers le programme Erasmus +.</p>
+						</div>
+						<div class=tab-pane id=partner-4>
+							<h3>Ministère de la Ville, de la Jeunesse et des Sports</h3>
+							<p>Le ministère de la Ville, de la Jeunesse et des Sports fait partie des partenaires institutionnels de longue date du PEJ-France. C’est aussi à travers d’un financement régulier que le ministère assure le soutien des actions du Parlement Européen des Jeunes en France.</p>
+						</div>
+						<div class=tab-pane id=partner-5>
+							<h3>Ministère de l’Education nationale, de l’Enseignement supérieur et de la Recherche</h3>
+							<p>Le ministère de l’Education nationale, de l’Enseignement supérieur et de la Recherche fait partie des partenaires institutionnels de longue date du PEJ-France. C’est aussi à travers d’un financement régulier que le ministère assure le soutien des actions du Parlement Européen des Jeunes en France.</p>
+						</div>
+						<div class=tab-pane id=partner-6>
+							<h3>Ministère des Affaires étrangères et du Développement international</h3>
+							<p>A travers le secrétariat d’Etat aux Affaires européennes, le ministère des Affaires étrangères et du Développement international fait partie des partenaires institutionnels de longue date du PEJ-France. C’est aussi à travers d’un financement régulier que le ministère assure le soutien des actions du Parlement Européen des Jeunes en France.</p>
+						</div>
+						<div class=tab-pane id=partner-7>
+							<h3>Fondation Hippocrène</h3>
+							<p>La Fondation Hippocrène est une fondation d'utilité publique dont la mission principale est de « Vivre l'Europe » en renforçant la cohésion entre les jeunes Européens. Ayant des objectifs similaires au PEJ-France, la fondation Hippocrène assure un soutien régulier des événements du Parlement Européen des Jeunes.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section id=contact>
+			<div class=container>
+				<h2>contact</h2>
+				<div class="form col-md-6 col-xs-12">
+					<div class=form-response>Message envoyé !</div>
+					<form>
+						<div class=form-group><input class=form-control id=name name=name placeholder=Nom required=true /></div>
+						<div class=form-group><input class=form-control id=email name=email placeholder=Email required=true type=email /></div>
+						<div class=form-group><input class=form-control id=subject name=subject placeholder=Sujet required=true /></div>
+						<div class=form-group><textarea class=form-control id=message name=message placeholder="Votre message" required=true rows=7></textarea></div>
+						<button class="btn btn-primary pull-right" id=contact-submit type=submit>Envoyer</button>
+					</form>
+				</div>
+				<div class="infos col-md-6 col-xs-12">
+					<ul class=pej>
+						<li><a href="http://www.pejfrance.org/comite-grand-ouest/" target=_blank><img alt="" src="<?php bloginfo('template_url'); ?>/images/home/logo-grand-ouest.png"/></a></li>
+						<li><a href="http://www.pejfrance.org/" target=_blank><img alt="" src="<?php bloginfo('template_url'); ?>/images/home/logo-pej.png"/></a></li>
+					</ul>
+					<div class=social>
+						<h3>Retrouvez-nous sur les réseaux sociaux !</h3>
+						<div><img alt="" src="<?php bloginfo('template_url'); ?>/images/home/facebook.png"/><a class=link href="https://www.facebook.com/rise2016">RISE 2016</a><span>|</span><a class=link href="https://www.facebook.com/pejfrance">PEJ France</a><span>|</span><a class=link href="https://www.facebook.com/ComiteGrandOuest">Comité Grand-Ouest</a></div>
+						<button ,="" class="btn btn-primary" data-target="#myModal" data-toggle=modal>Inscrivez-vous à la newsletter</button>
+					</div>
+				</div>
+			</div>
+		</section>
+		<footer id=footer>
+			<div class=container>
+				<p>Le PEJ-France est une association non-partisane d’éducation à la citoyenneté européenne, association loi 1901 d’intérêt général agréée Jeunesse et Éducation Populaire et Complémentaire de l’Enseignement public.</p>
+				<p><a data-target="#modal_mention" data-toggle=modal> Mentions légales</a></p>
+			</div>
+		</footer>
+		<div aria-labelledby=modal_mentionLabel class="modal fade" id=modal_mention role=dialog tabindex=-1>
+			<div class=modal-dialog role=document>
+				<div class=modal-content>
+					<div class=modal-header>
+						<button aria-label=Close class=close data-dismiss=modal type=button><span aria-hidden=true> &times;</span></button>
+						<h4 class=modal-title id=modal_mentionLabel>Mentions légales</h4>
+					</div>
+					<div class=modal-body>
+						<h3>Raison Sociale : </h3>
+						<p>Le Comité Grand Ouest du Parlement Européen des Jeunes est une association loi 1901, enregistrée à la préfecture de Rennes en 2015, Numéro de SIRET : 812 489 144 00012</p>
+						<h3>Siège Social : </h3>
+						<p> <strong>Comité Grand Ouest – PEJ France</strong> <br>10 Place du Parlement de Bretagne <br>35000 Rennes <br>Mail : <a href='mailto:comitegrandouest@pejfrance.org'>comitegrandouest@pejfrance.org</a> <br><strong>Directeurs de la publication :</strong> <br>Sugirthan Sivarajah & William Goyet</p>
+						<h3>Développement WEB : </h3>
+						<p> Charles Prévôt <br>Léna Defay <br>Valentin Galmand <br>Mathieu Débit </p>
+						<p> <strong>Sécurité.</strong> L’accès aux données personnelles de chaque adhérent se fait au moyen d’un mot de passe individuel. Le PEJ-France est garant de la sécurité des données de ses membres et prend toutes précautions utiles afin de préserver la sécurité de ces données, et notamment d’empêcher qu’elles soient déformées, endommagées, ou que des tiers non autorisés y aient accès.</p>
+						<p>Conformément à la loi, le PEJ-France exclut toute utilisation commerciale ou politique des données traitées. Les données susceptibles de faire l’objet d’un traitement sont les données relatives à l’identité, la vie professionnelle et les centres d’intérêts (à l’exception des données telles que les origines raciales ou ethniques, les opinions politiques, philosophiques ou religieuses, l’appartenance syndicale, l’état de santé ou la vie sexuelle).</p>
+						<p><strong>Droit d’accès et de rectification des données</strong> Conformément à la loi « informatique et libertés » du 6 janvier 1978 modifiée en 2004, vous bénéficiez d’un droit d’accès, de modification, de rectification et de suppression des données personnelles qui vous concernent. Vous pouvez exercer ce droit en vous adressant à : </p>
+						<br>
+						<p> <strong>Comité Grand Ouest – PEJ France</strong> <br>10 Place du Parlement de Bretagne <br>35000 Rennes <br>Mail : <a href='mailto:comitegrandouest@pejfrance.org'>comitegrandouest@pejfrance.org</a></p>
+					</div>
+					<div class="modal fade" id=myModal role=dialog>
+						<div class=modal-dialog>
+							<div class=modal-content>
+								<div class=modal-header>
+									<button class=close data-dismiss=modal type=button> &times;</button>
+									<h4 class=modal-title>Restez informé !</h4>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/waypoints/2.0.3/waypoints.min.js"></script>
+    <script src="<?php bloginfo('template_url'); ?>/js/home/all.js"></script>
+    <script src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"></script>
+	</body>
+</html>
